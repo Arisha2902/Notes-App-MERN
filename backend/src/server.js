d0b2689ac4e6a8cee438ec80c014bc522aa9fa22
Arisha Firoz <169101584+Arisha2902@users.noreply.github.com>
2025-10-01 01:32:58 +0530

    ssm

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 
 dotenv.config();
@@ -12,10 +13,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 // connectDB();
 
 // middleware
+if(process.env.NODE_ENV !== "production"){
+
+
 app.use(
     cors({
         origin: "http://localhost:5173",
@@ -24,6 +29,7 @@ app.use(
 app.use(express.json())
 app.use(rateLimiter)
 
+}
 // simple middleware
 // app.use((req,res,next) => {
 //     console.log("we just got a new req")
@@ -31,7 +37,17 @@ app.use(rateLimiter)
 //     next();
 // })
 
+if(process.env.NODE_ENV === "production"){
+    
 app.use("/api/notes", notesRoutes);
+
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get("*" ,(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 
 // app.get("/api/notes" , (req,res) => {
 // res.send("you got your notes there");
